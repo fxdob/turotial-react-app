bc98732434c7da93681310fea856f516f7e4a7d8
fxdo <97800192+fxdob@users.noreply.github.com>
2022-05-17 16:05:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import Table from './Table'
 import Form from './Form'
 
 class App extends Component {
-  state = { characters: [], };
-  //const { characters } = this.state
-  removeCharacter = (index) => {
-    const {characters} = this.state
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
-  return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-      </div>
-   )
-}
-//render() {}
+  state = {
+        characters: []
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+    
+        this.setState({
+            characters: characters.filter((character, i) => { 
+                return i !== index;
+            })
+        });
+    }
+
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render() {
+        const { characters } = this.state;
+        
+        return (
+            <div className="container">
+              <Table characterData={characters} removeCharacter={this.removeCharacter} />
+              <Form />
+            </div>
+        );
+    }
 }
 
 
