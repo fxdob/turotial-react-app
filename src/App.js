f0f359de516b8d2c9581f62b8397fd852076b2cf
fxdo <97800192+fxdob@users.noreply.github.com>
2022-05-17 15:41:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,48 +3,26 @@ import Table from './Table'
 import Form from './Form'
 
 class App extends Component {
-  state = {
-    characters: [],
-  }
+  state = { characters: [], }
   removeCharacter = (index) => {
-  const {characters} = this.state
-
-  this.setState({
-    characters: characters.filter((character, i) => {
-      return i !== index
-    }),
-  })
+    const {characters} = this.state
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
   }
   render() {
-  const { characters } = this.state
+    const { characters } = this.state
 
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={this.removeCharacter} />
-    </div>
-  )
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    )
+  }
 }
 
 
-
-/*class App extends Component {
-  state = {
-    characters:[],
-  }
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={this.removeCharacter} />
-      <Form />
-    </div>
-  )
-  removeCharacter = index => {
-      const {characters} = this.state
-      this.setState({
-        characters:characters.filter((character, i) => {
-        return i !== index 
-            }),
-        })
-  }
-}*/
 
 export default App
